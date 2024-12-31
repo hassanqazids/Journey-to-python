c1f28aa361f62dada11632409cd524ee3e2dcb89
Hassan Qazi <hassanqazids@gmail.com>
2025-01-01 03:08:04 +0500

    Add files via upload

---
+++ python dictionary/Python dictionary methods.py
@@ -0,0 +1,52 @@
+# #Dictionary in python:
+# # A dictionary is an unordered , mutable, store the data in key-value pairs.
+# # key must be unique and immutable , while values can be of any type .
+
+# #Example
+
+# mydict = {"name": "Hassan ", "age":"18","city":"Hyd"}
+# print(mydict["name"]) #output : Hassan
+
+# #Adding or updating items
+# mydict["city"] = "Karachi,Sindh"
+# mydict["Porfession"] ="Data Science And Web Developer"
+# print(mydict)
+
+# #create mydict
+# #updating mydict // city,Add one item Profession
+# #del items in mydict
+
+# del mydict["city"]
+# print(mydict)
+
+
+# #iterating thought a dictionary (for loop)
+# # for loop
+# # for key,value in mydict.items():
+# #     print(f"{key} : {value}")
+
+# #iterating thought a dictionary (While loop)
+# #Wile loop
+# # items = list(mydict.items())
+# # i=0
+# # while i < len(items):
+# #     key,value = items[i]
+# #     print(f"{key} : {value}")
+# #     i+=1
+
+# # //message
+# #list Hassan = Full Stack Developer:
+# #list = ["Project web development ,(PHP,MYSQL ,JAVASCRIPT(ReactJS,Vuejs ,Augular ,Firebase ,superbase ,mogodb,ExpressJS ,Nodejs ), TAILWIND CSS , DATASCIENT PYTHON (oop ,) , "
+# #"numpy , Panda ,Web scaping, Chrome ,Py ,SImple automation ,MODEL AI ,ML , DL, GUI , DJANGO  PYTHON_PROJECT ,R :  data Sturture(list,set,tuples ,if-esle , loops) " ]
+
+# #custom project using list ,set,tuple,if-esle and loop
+# #project name : Grocery organizer
+
+# #objective :  buit a program to organizer a Grocery list where the user add ,remove or check organizer items.
+
+# #Features :
+# #use a list to store Grocery items
+# #use a set to store the unique categories of Groceries
+# #use  loop for continuous interaction
+# #usng if-esle to check the input and perform action
+
