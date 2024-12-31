53d9e77ba3e8454d9847a3585e255249c7cc73c5
Hassan Qazi <hassanqazids@gmail.com>
2025-01-01 03:05:08 +0500

    Add files via upload

---
+++ List methods/restart.py
@@ -0,0 +1,42 @@
+
+#List and its methods
+#in python array does not exits but we have list set dictionary and tuple
+#list can be changed after it is made
+#example of list
+#list = [1,2,3,4,5]
+
+#accessing element
+#list = [1,2,3,4]
+#print(list[2])
+
+
+#Methods
+#Append use to add
+#mylist=['var1','var2','var3','var4']
+#mylist.append('var5')
+#print(mylist)
+
+#mylist =['1','2','3']
+#mylist.clear()
+#print(mylist)
+
+#pop use to remove
+
+#mylist=['hell','dell','hp']
+#mylist.pop(0)
+#print(mylist)
+
+#copy method use to copy the list
+#mylist = ['apple','apple2','apple3']
+#mylist.copy()
+#print(mylist)
+
+#.remove use to remove element
+#mylist=['cherry','apple','fumble']
+#mylist.remove('cherry')
+#print(mylist)
+
+#reverse use to reverse
+#mylist = ['cherry1','cherry2','cherry3']
+#mylist.reverse()
+#print(mylist)
